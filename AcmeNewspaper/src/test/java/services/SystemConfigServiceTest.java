62d0a6fb0ea16d0bff079769bd7058dc785923d0
Rafael Trujillo <raftrugon@alum.us.es>
2018-04-08 09:19:12 +0200

    System config y configuracion de test

--- AcmeNewspaper/src/test/java/services/SystemConfigServiceTest.java
+++ AcmeNewspaper/src/test/java/services/SystemConfigServiceTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import utilities.AbstractTest;
+import domain.Admin;
+import domain.SystemConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -22,52 +24,115 @@ public class SystemConfigServiceTest extends AbstractTest {
 	@Autowired
 	private SystemConfigService	systemConfigService;
 
-
 	//Supporting services -----------------------------------------------------
 
+	@Autowired
+	private AdminService		adminService;
+
+
 	// Tests ------------------------------------------------------------------
+
+	// =====================================================================================================================
+	// ===================================================== FIND GET ======================================================
+	// =====================================================================================================================
+
 	@Test
-	public void driverFindOne() {
+	public void testGetDriver() {
 
 		System.out.println("===============================================================================================================");
-		System.out.println("=====================================TEST FIND ONE SYSTEM CONFIG==================================================");
+		System.out.println("===============================================TEST GET===================================================");
 		System.out.println("===============================================================================================================\r");
 
 		Object testingData[][] = {
-			//Positive test
 			{
-				getEntityId("systemConfig1"), null, "Búsqueda del find One correcto"
-			},
-
-			//Find one newspaper using another role id
-			{
-				getEntityId("user1"), IllegalArgumentException.class, "Intentando buscar un user"
-			},
+				null, "Busqueda de system."
+			}
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			templateFindOne((int) testingData[i][0], (Class<?>) testingData[i][1], (String) testingData[i][2]);
+			this.testGetTemplate((Class<?>) testingData[i][0], (String) testingData[i][1]);
 	}
 
-	protected void templateFindOne(int systemConfigId, Class<?> expected, String explanation) {
+	protected void testGetTemplate(final Class<?> expected, final String explanation) {
 
 		Class<?> caught = null;
 
 		try {
-			systemConfigService.findOne(systemConfigId);
+			super.authenticate(null);
 
+			@SuppressWarnings("unused")
+			SystemConfig sc = this.systemConfigService.get();
+
+			super.unauthenticate();
 		} catch (Throwable oops) {
 			caught = oops.getClass();
 		}
 
-		checkExceptions(expected, caught);
+		this.checkExceptions(expected, caught);
+
+		// --------------------------------- CONSOLA ---------------------------------
 
 		if (expected == null)
 			System.out.println("---------------------------- POSITIVO ---------------------------");
 		else
 			System.out.println("---------------------------- NEGATIVO ---------------------------");
 		System.out.println("Explicación: " + explanation);
-		System.out.println("SystemConfigId: " + systemConfigId);
+		System.out.println("¿Objeto encontrado? " + (caught == null));
+		System.out.println("\r¿Correcto? " + (expected == caught));
+		System.out.println("-----------------------------------------------------------------\r");
+
+	}
+
+	// =====================================================================================================================
+	// =================================================== SAVE system config ==================================================
+	// =====================================================================================================================
+
+	@Test
+	public void testSystemConfigDriver() {
+
+		System.out.println("===============================================================================================================");
+		System.out.println("========================================system config====================================================");
+		System.out.println("===============================================================================================================\r");
+
+		Object testingData[][] = {
+			{
+				"admin", "pene", null, "Correcta creacion system."
+			}, {
+				"user1", "pene", IllegalArgumentException.class, "SAVE con otro rol."
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testSystemConfigTemplate(super.getEntityId((String) testingData[i][0]), (String) testingData[i][1], (Class<?>) testingData[i][2], (String) testingData[i][3]);
+	}
+
+	protected void testSystemConfigTemplate(final int adminAux, String taboo, final Class<?> expected, final String explanation) {
+
+		Class<?> caught = null;
+
+		try {
+			Admin admin = adminService.findOne(adminAux);
+			super.authenticate(admin.getUserAccount().getUsername());
+
+			SystemConfig sc = systemConfigService.get();
+			sc.getTabooWordss().add(taboo);
+
+			this.systemConfigService.save(sc);
+
+			super.unauthenticate();
+		} catch (Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
+
+		// --------------------------------- CONSOLA ---------------------------------
+
+		if (expected == null)
+			System.out.println("---------------------------- POSITIVO ---------------------------");
+		else
+			System.out.println("---------------------------- NEGATIVO ---------------------------");
+		System.out.println("Explicación: " + explanation);
 		System.out.println("\r¿Correcto? " + (expected == caught));
 		System.out.println("-----------------------------------------------------------------\r");
 
