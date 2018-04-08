d5bf3df9b8f56beac284a04f3395194415f41f15
pablotabares <pablo.tabaresgarcia@gmail.com>
2018-04-08 20:53:33 +0200

    Fix: future articles issue
    Article groups wich are ready to be published but publicationMoment is still future are no longer displayed

--- AcmeNewspaper/src/main/java/repositories/ArticleRepository.java
+++ AcmeNewspaper/src/main/java/repositories/ArticleRepository.java
@@ -13,16 +13,16 @@ import domain.Newspaper;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
-	@Query(value="select * from article as a where exists (select n_id,a_date from (select sum(finalMode)-count(id) as published, publicationMoment as a_date, newspaper_id as n_id from article group by n_id,a_date) as temporal1 where published = 0 and n_id = a.newspaper_id and a_date = a.publicationMoment) and a.inappropriate = false", nativeQuery=true)
+	@Query(value="select * from article as a where exists (select n_id,a_date from (select sum(finalMode)-count(id) as published, publicationMoment as a_date, newspaper_id as n_id from article group by n_id,a_date) as temporal1 where published = 0 and n_id = a.newspaper_id and a_date = a.publicationMoment) and a.inappropriate = false and a.publicationMoment < CURRENT_TIMESTAMP", nativeQuery=true)
 	Collection<Article> findAllPublished();
 	
-	@Query(value="select * from article as a where exists (select n_id,a_date from (select sum(finalMode)-count(id) as published, publicationMoment as a_date, newspaper_id as n_id from article group by n_id,a_date) as temporal1 where published = 0 and n_id = a.newspaper_id and a_date = a.publicationMoment) and (title like %?1% or summary like %?1% or body like %?1%) and a.inappropriate = false", nativeQuery=true)
+	@Query(value="select * from article as a where exists (select n_id,a_date from (select sum(finalMode)-count(id) as published, publicationMoment as a_date, newspaper_id as n_id from article group by n_id,a_date) as temporal1 where published = 0 and n_id = a.newspaper_id and a_date = a.publicationMoment) and (title like %?1% or summary like %?1% or body like %?1%) and a.inappropriate = false and a.publicationMoment < CURRENT_TIMESTAMP", nativeQuery=true)
 	Collection<Article> findAllPublishedKeyword(String keyword);
 	
-	@Query(value="select a.* from article as a join newspaper on a.newspaper_id = newspaper.id where exists (select n_id,a_date from (select sum(finalMode)-count(id) as published, publicationMoment as a_date, newspaper_id as n_id from article group by n_id,a_date) as temporal1 where published = 0 and n_id = a.newspaper_id and a_date = a.publicationMoment) and newspaper.user_id = ?1 and a.inappropriate = false", nativeQuery=true)
+	@Query(value="select a.* from article as a join newspaper on a.newspaper_id = newspaper.id where exists (select n_id,a_date from (select sum(finalMode)-count(id) as published, publicationMoment as a_date, newspaper_id as n_id from article group by n_id,a_date) as temporal1 where published = 0 and n_id = a.newspaper_id and a_date = a.publicationMoment) and newspaper.user_id = ?1 and a.inappropriate = false and a.publicationMoment < CURRENT_TIMESTAMP", nativeQuery=true)
 	Collection<Article> findAllPublishedForUser(int userId);
 	
-	@Query(value="select a.* from article as a join newspaper on a.newspaper_id = newspaper.id where exists (select n_id,a_date from (select sum(finalMode)-count(id) as published, publicationMoment as a_date, newspaper_id as n_id from article group by n_id,a_date) as temporal1 where published = 0 and n_id = a.newspaper_id and a_date = a.publicationMoment) and newspaper.id = ?1 and a.inappropriate = false", nativeQuery=true)
+	@Query(value="select a.* from article as a join newspaper on a.newspaper_id = newspaper.id where exists (select n_id,a_date from (select sum(finalMode)-count(id) as published, publicationMoment as a_date, newspaper_id as n_id from article group by n_id,a_date) as temporal1 where published = 0 and n_id = a.newspaper_id and a_date = a.publicationMoment) and newspaper.id = ?1 and a.inappropriate = false and a.publicationMoment < CURRENT_TIMESTAMP", nativeQuery=true)
 	Collection<Article> findAllPublishedForNewspaper(int newspaperId);
 
 	@Modifying
