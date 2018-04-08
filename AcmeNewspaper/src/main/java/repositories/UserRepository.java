dbba13755f255aeb84fdf5acdaea1270c2605ce2
pablotabares <pablo.tabaresgarcia@gmail.com>
2018-04-08 14:02:53 +0200

    Add: 7.3 (WIP)

--- AcmeNewspaper/src/main/java/repositories/UserRepository.java
+++ AcmeNewspaper/src/main/java/repositories/UserRepository.java
@@ -12,5 +12,11 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	@Query("select u from User u where u.userAccount.id = ?1")
 	User findByUserAccount(int id);
+	
+	
+	@Query("select coalesce(avg(u.newspapers.size),0), coalesce(stddev(u.newspapers.size),0) from User u")
+	Double[] getNewspaperUserStats();
+	
+	
 
 }
